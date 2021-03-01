39dcacfa087be2770b07f1713b3b58d5bd918824
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-03-01 12:01:36 +0530

    Changes related to Email triggering component

---
+++ src/main/java/com/capeelectric/util/EmailUtil.java
@@ -0,0 +1,11 @@
+package com.capeelectric.util;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailUtil {
+	
+	private JavaMailSender sender;
+
+}
