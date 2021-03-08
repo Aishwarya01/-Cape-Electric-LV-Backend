c27326b861d892e01ded523ddc3d406d75a22957
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-03-08 15:22:12 +0530

    Added Code to handle jwt

--- src/main/java/com/capeelectric/controller/UserController.java
+++ src/main/java/com/capeelectric/controller/UserController.java
@@ -7,6 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +21,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.capeelectric.config.JwtTokenUtil;
 import com.capeelectric.exception.UserException;
-import com.capeelectric.model.CustomUserDetails;
 import com.capeelectric.model.User;
 import com.capeelectric.request.AuthenticationRequest;
+import com.capeelectric.response.AuthenticationResponse;
 import com.capeelectric.service.impl.CustomUserDetailsService;
 import com.capeelectric.service.impl.UserDetailsServiceImpl;
 
@@ -39,6 +45,12 @@ public class UserController {
 	@Autowired
 	private UserDetailsServiceImpl userService;
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	
 	@PostMapping("/registerUser")
 	public ResponseEntity<Void> addUser(@RequestBody User user) throws UserException {
 		logger.debug("Add User starts");
@@ -50,8 +62,15 @@ public class UserController {
 	}
 	
 	@PostMapping("/authenticate")
-	public CustomUserDetails fetchUser(@RequestBody AuthenticationRequest request) throws UserException {
-		return userService.loadUserInformation(userDetailsService.loadUserByUsername(request.getEmail()), request.getPassword());
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+		logger.debug("Create Authenticate Token starts");
+		authenticate(authenticationRequest.getEmail(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+		logger.debug("Create Authenticate Token ends");
+		return ResponseEntity.ok(new AuthenticationResponse(token));
 	}
 	
 	@GetMapping("/forgotPassword/{email}")
@@ -65,5 +84,15 @@ public class UserController {
 		User user  = userService.updateUser(request.getEmail(), request.getPassword());
 		logger.debug("Update User ends");
 		return new ResponseEntity<String>(user.getUserName(), HttpStatus.OK);
+	}
+	
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
 	}
 }
