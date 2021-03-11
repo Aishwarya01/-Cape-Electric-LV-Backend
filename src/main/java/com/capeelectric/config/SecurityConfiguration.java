258c9d641a6a5b68188303e55cefd6173aa29948
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-03-11 19:45:09 +0530

    Reverted the changes

--- src/main/java/com/capeelectric/config/SecurityConfiguration.java
+++ src/main/java/com/capeelectric/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -68,7 +69,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		httpSecurity.cors().and()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/api/v1/authenticate"
-						).permitAll()
+						,
+						"/api/v1/registerUser",
+						"/api/v1/forgotPassword/**",
+						"/api/v1/updatePassword/**",
+						"/api/v1/changePassword/**",
+						"/api/v1/retrieveUserInformation").permitAll()
 				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
@@ -89,5 +95,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
 			}
 		};
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring().antMatchers("/api/v1/registerUser");
 	}
 }
