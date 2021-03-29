5902f16469602b53d3a24a8b71cfaba824ae6daa
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-03-29 17:17:01 +0530

    Added id in the insert query for application types

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into applicationtypes values ('Verification Of LV Systems (IEC 60364-6)');
-insert into applicationtypes values ('Verification of HV system (up to 33 kV) (IEC 61936-1)');
-insert into applicationtypes values ('Lightning protection conformity assessment, risk assessment, inspection and maintenance (IEC 62305-3 & 4)');
-insert into applicationtypes values ('EMC assessment of an installation (IEC 61000-5-1)');
-insert into applicationtypes values ('Failure analysis of electronic systems');
-insert into applicationtypes values ('Conformity and project analysis');
+insert into applicationtypes values (1,'Verification Of LV Systems (IEC 60364-6)');
+insert into applicationtypes values (2,'Verification of HV system (up to 33 kV) (IEC 61936-1)');
+insert into applicationtypes values (3,'Lightning protection conformity assessment, risk assessment, inspection and maintenance (IEC 62305-3 & 4)');
+insert into applicationtypes values (4,'EMC assessment of an installation (IEC 61000-5-1)');
+insert into applicationtypes values (5,'Failure analysis of electronic systems');
+insert into applicationtypes values (6,'Conformity and project analysis');
 
 create table users 
 (user_id integer not null, 
