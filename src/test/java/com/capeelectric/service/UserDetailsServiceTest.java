b6d0102c4cbe0de979b74f66fc9899601c3a0d93
thirumoorthy2005 <79498283+thirumoorthy2005@users.noreply.github.com>
2021-04-08 12:17:48 +0530

    code_alignment

--- src/test/java/com/capeelectric/service/UserDetailsServiceTest.java
+++ src/test/java/com/capeelectric/service/UserDetailsServiceTest.java
@@ -1,6 +1,6 @@
 package com.capeelectric.service;
 
- import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
@@ -25,23 +25,22 @@ import com.capeelectric.service.impl.UserDetailsServiceImpl;
 
 @ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
-public class UserDetailsServiceTest   {
-	
+public class UserDetailsServiceTest {
+
 	@MockBean
 	private UserRepository userRepository;
-	
+
 	@InjectMocks
 	private UserDetailsServiceImpl userDetailsServiceImpl;
-	 
- 	@MockBean
- 	private BCryptPasswordEncoder passwordEncoder;
- 	
+
+	@MockBean
+	private BCryptPasswordEncoder passwordEncoder;
+
 	private User user;
-	
+
 	private Optional<User> optionaluser;
-	
- 	 
-	 {
+
+	{
 		user = new User();
 		user.setUsername("moorthy@capeindia.net");
 		user.setPassword("moorthy");
@@ -62,7 +61,7 @@ public class UserDetailsServiceTest   {
 		UserException assertThrows = Assertions.assertThrows(UserException.class,
 				() -> userDetailsServiceImpl.findByUserName(null));
 		assertEquals("Email is required", assertThrows.getMessage());
-		
+
 		when(userRepository.findByUsername("moorthy@capeindia.net")).thenReturn(null);
 		UserException assertThrows2 = Assertions.assertThrows(UserException.class,
 				() -> userDetailsServiceImpl.findByUserName("moorthy@capeindia.net"));
@@ -95,58 +94,58 @@ public class UserDetailsServiceTest   {
 		user.setUserexist(false);
 		Assertions.assertThrows(null,
 				() -> userDetailsServiceImpl.changePassword("moorthy@capeindia.net", "moorthy", "moorthy123"));
-	 
+
 	}
 
-	
-	  @Test 
-	  public void testUpdatedPassword() throws UserException {
- 
-			when(userRepository.findByUsername("moorthy@capeindia.net")).thenReturn(optionaluser);
+	@Test
+	public void testUpdatedPassword() throws UserException {
 
-			User updatePassword = userDetailsServiceImpl.updatePassword("moorthy@capeindia.net", "moorthy123");
-			assertNull(updatePassword);
+		when(userRepository.findByUsername("moorthy@capeindia.net")).thenReturn(optionaluser);
 
-			UsernameNotFoundException assertThrows = Assertions.assertThrows(UsernameNotFoundException.class,
-					() -> userDetailsServiceImpl.updatePassword(null, "moorthy123"));
-			assertEquals("username not valid", assertThrows.getMessage());
+		User updatePassword = userDetailsServiceImpl.updatePassword("moorthy@capeindia.net", "moorthy123");
+		assertNull(updatePassword);
 
-			user.setUserexist(false);
-			UserException assertThrows2 = Assertions.assertThrows(UserException.class,
-					() -> userDetailsServiceImpl.updatePassword("moorthy@capeindia.net", "moorthy123"));
-			assertEquals("User Not available", assertThrows2.getMessage());
+		UsernameNotFoundException assertThrows = Assertions.assertThrows(UsernameNotFoundException.class,
+				() -> userDetailsServiceImpl.updatePassword(null, "moorthy123"));
+		assertEquals("username not valid", assertThrows.getMessage());
 
-	  }
+		user.setUserexist(false);
+		UserException assertThrows2 = Assertions.assertThrows(UserException.class,
+				() -> userDetailsServiceImpl.updatePassword("moorthy@capeindia.net", "moorthy123"));
+		assertEquals("User Not available", assertThrows2.getMessage());
 
-		@Test
-		public void testSaveUser() throws UserException {
+	}
 
-			when(userRepository.findByUsername("moorthy@capeindia.net")).thenReturn(optionaluser);
-			UserException assertThrows = Assertions.assertThrows(UserException.class, () -> userDetailsServiceImpl.saveUser(user));
-			assertEquals("User already available", assertThrows.getMessage());
-			user.setUserexist(false);
-			User saveUser = userDetailsServiceImpl.saveUser(user);
-			assertNull(saveUser);
-			
-		}
-		
-		@Test
-		public void testRetrieveUserInformation() throws UserException {
-			when(userRepository.findByUsername("moorthy@capeindia.net")).thenReturn(optionaluser);
-			User retrieveUserInformation = userDetailsServiceImpl.retrieveUserInformation(user.getEmail());
-			assertEquals("moorthy@capeindia.net", retrieveUserInformation.getEmail());
+	@Test
+	public void testSaveUser() throws UserException {
 
-			user.setUserexist(false);
-			UserException assertThrows = Assertions.assertThrows(UserException.class,
-					() -> userDetailsServiceImpl.retrieveUserInformation(user.getEmail()));
-			assertEquals("User not available", assertThrows.getMessage());
+		when(userRepository.findByUsername("moorthy@capeindia.net")).thenReturn(optionaluser);
+		UserException assertThrows = Assertions.assertThrows(UserException.class,
+				() -> userDetailsServiceImpl.saveUser(user));
+		assertEquals("User already available", assertThrows.getMessage());
+		user.setUserexist(false);
+		User saveUser = userDetailsServiceImpl.saveUser(user);
+		assertNull(saveUser);
 
-		}
+	}
 
-		@Test
-		public void testUpdateUserProfile() {
-			User updateUserProfile = userDetailsServiceImpl.updateUserProfile(user);
-			assertNull(updateUserProfile);
-		}
-	 
+	@Test
+	public void testRetrieveUserInformation() throws UserException {
+		when(userRepository.findByUsername("moorthy@capeindia.net")).thenReturn(optionaluser);
+		User retrieveUserInformation = userDetailsServiceImpl.retrieveUserInformation(user.getEmail());
+		assertEquals("moorthy@capeindia.net", retrieveUserInformation.getEmail());
+
+		user.setUserexist(false);
+		UserException assertThrows = Assertions.assertThrows(UserException.class,
+				() -> userDetailsServiceImpl.retrieveUserInformation(user.getEmail()));
+		assertEquals("User not available", assertThrows.getMessage());
+
+	}
+
+	@Test
+	public void testUpdateUserProfile() {
+		User updateUserProfile = userDetailsServiceImpl.updateUserProfile(user);
+		assertNull(updateUserProfile);
+	}
+
 }
