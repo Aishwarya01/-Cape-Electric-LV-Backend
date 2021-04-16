b8604f86862ef87032e1a0f2d7eb512f8dc6fad5
thirumoorthy2005 <79498283+thirumoorthy2005@users.noreply.github.com>
2021-04-16 21:00:44 +0530

    re_commit

---
+++ src/main/java/com/capeelectric/model/Site.java
@@ -0,0 +1,254 @@
+package com.capeelectric.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
+
+
+
+/**
+ * The persistent class for the site_table database table.
+ * 
+ */
+@Entity
+@Table(name = "site_table")
+@NamedQueries(value = {
+		 @NamedQuery(name = "siteRepository.findByUserNameAndClientNameAndSite", query = "select s from Site s where s.userName=:userName and s.clientName=:clientName and s.departmentName=:departmentName")
+})
+public class Site implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "SITE_ID")
+	private Integer siteId;
+
+	@Column(name = "USER_NAME")
+	private String userName;
+
+ 	@Column(name = "CLIENT_NAME")
+	private String clientName;
+
+	@Column(name = "DEPARTMENT_NAME")
+ 	private String departmentName;
+
+	@Column(name = "SITE")
+ 	private String site;
+
+	@Column(name = "PERSON_INCHARGE")
+	private String personIncharge;
+	
+	@Column(name = "E_MAIL")
+ 	private String email;
+
+	@Column(name = "ADDRESSLINE_1")
+	private String addressLine_1;
+
+	@Column(name = "ADDRESSLINE_2")
+	private String addressLine_2;
+
+	@Column(name = "LAND_MARK")
+	private String landMark;
+
+	@Column(name = "CITY")
+	private String city;
+
+	@Column(name = "STATE")
+	private String state;
+
+	@Column(name = "COUNTRY")
+	private String country;
+
+	@Column(name = "ZIP_CODE")
+	private String zipCode;
+
+	@Column(name = "PHONE_NUMBER")
+	private String phoneNumber;
+
+	@Column(name = "CREATED_BY")
+	private String createdBy;
+
+	@Column(name = "CREATED_DATE")
+	private LocalDateTime createdDate;
+
+	@Column(name = "UPDATED_BY")
+	private String updatedBy;
+
+	@Column(name = "UPDATED_DATE")
+	private LocalDateTime updatedDate;
+	
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@ManyToOne
+	@JoinColumn(name="DEPARTMENT_ID")
+	private Department department;
+
+	public Integer getSiteId() {
+		return siteId;
+	}
+
+	public void setSiteId(Integer siteId) {
+		this.siteId = siteId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+
+	public String getPersonIncharge() {
+		return personIncharge;
+	}
+
+	public void setPersonIncharge(String personIncharge) {
+		this.personIncharge = personIncharge;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddressLine_1() {
+		return addressLine_1;
+	}
+
+	public void setAddressLine_1(String addressLine_1) {
+		this.addressLine_1 = addressLine_1;
+	}
+
+	public String getAddressLine_2() {
+		return addressLine_2;
+	}
+
+	public void setAddressLine_2(String addressLine_2) {
+		this.addressLine_2 = addressLine_2;
+	}
+
+	public String getLandMark() {
+		return landMark;
+	}
+
+	public void setLandMark(String landMark) {
+		this.landMark = landMark;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+}
---
+++ src/main/java/com/capeelectric/repository/CompanyRepository.java
@@ -0,0 +1,16 @@
+package com.capeelectric.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capeelectric.model.Company;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
+	List<String> findByClientName(String clientName);
+
+	List<Company> findByUserName(String userName);
+
+}
