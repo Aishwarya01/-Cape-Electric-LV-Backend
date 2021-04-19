a41d35faeace489a93534587a57386e2ecd15792
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-04-19 19:02:56 +0530

    Added internal server exception handling in Global Exception Handler

--- src/main/java/com/capeelectric/exception/GlobalExceptionHandler.java
+++ src/main/java/com/capeelectric/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.capeelectric.exception;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 import com.capeelectric.util.ErrorMessage;
 
@@ -20,6 +24,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorMessage> handleChangePasswordException(ChangePasswordException e){
     		ErrorMessage exceptionMessage = new ErrorMessage(e.getMessage(), e.getLocalizedMessage(), "406");
     		return new ResponseEntity<ErrorMessage>(exceptionMessage, new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE);
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorMessage> handleAllExceptions(Exception ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorMessage error = new ErrorMessage(ex.getMessage(), ex.getLocalizedMessage(), "500");
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
     
 }
