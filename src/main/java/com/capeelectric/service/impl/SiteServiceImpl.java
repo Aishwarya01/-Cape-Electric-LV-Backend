ecce4564f1c80d22f73364bee30944c127513107
thirumoorthy2005 <79498283+thirumoorthy2005@users.noreply.github.com>
2021-05-07 12:52:34 +0530

    EmptyResultDataAccessException_throw_issue_fixed

--- src/main/java/com/capeelectric/service/impl/SiteServiceImpl.java
+++ src/main/java/com/capeelectric/service/impl/SiteServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.capeelectric.exception.CompanyDetailsException;
@@ -137,7 +138,7 @@ public class SiteServiceImpl implements SiteService {
 	 * then site_object will be delete
 	 */
 	@Override
-	public void deleteSite(Integer siteId) throws CompanyDetailsException {
+	public void deleteSite(Integer siteId) throws CompanyDetailsException,EmptyResultDataAccessException {
 		if (siteId != null && siteId != 0) {
 			Optional<Site> site = siteRepository.findById(siteId);
 
