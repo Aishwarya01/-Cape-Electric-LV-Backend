ae5ddf173828d77b0defa736e04c2407f73c8187
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-06-16 13:41:53 +0530

    REmoved the logic related to sendgrid and changes related to state repository code to display

--- src/main/java/com/capeelectric/repository/StateRepository.java
+++ src/main/java/com/capeelectric/repository/StateRepository.java
@@ -10,6 +10,6 @@ import com.capeelectric.model.State;
 
 @Repository
 public interface StateRepository extends CrudRepository<State, Integer> {
-	@Query(value = "select * from State_Table where Country_ID = (select country_id from Country_Table where code=?)", nativeQuery = true)
+	@Query(value = "select * from state_table where country_id = (select country_id from country_table where code=?)", nativeQuery = true)
 	List<State> fetchStatesByCountryCode(String code);
 }
