d0e508c4c22b775a1cb8aae7fbedc99fc2b0698d
thirumoorthy2005 <79498283+thirumoorthy2005@users.noreply.github.com>
2021-06-28 16:44:07 +0530

    twilio_for_sms_email verification

---
+++ src/main/java/com/capeelectric/config/TwilioInitiazer.java
@@ -0,0 +1,20 @@
+package com.capeelectric.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import com.capeelectric.model.Twilioproperties;
+import com.twilio.Twilio;
+
+@Configuration
+public class TwilioInitiazer {
+
+	private final Twilioproperties twilioproperties;
+
+	@Autowired
+	public TwilioInitiazer(Twilioproperties twilioproperties) {
+		this.twilioproperties = twilioproperties;
+		Twilio.init(twilioproperties.getAccountSid(), twilioproperties.getAuthToken());
+		System.out.println("Twilio initialized with account-" + twilioproperties.getAccountSid());
+	}
+}
