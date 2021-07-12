7b2f4a719f9b0a88db5e22f93aa831db2830f81f
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-07-12 12:38:29 +0530

    Added Code to implement Serializable in Country and State

--- src/main/java/com/capeelectric/model/Country.java
+++ src/main/java/com/capeelectric/model/Country.java
@@ -1,5 +1,6 @@
 package com.capeelectric.model;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -14,7 +15,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "COUNTRY_TABLE")
-public class Country {
+public class Country implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "COUNTRY_ID")
