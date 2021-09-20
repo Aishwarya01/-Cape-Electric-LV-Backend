7e43c73bf844ede745c858044fa4f903db432051
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-09-20 09:45:55 +0530

    Changes to arrest email sending to invalid users

--- src/main/java/com/capeelectric/service/impl/AWSEmailService.java
+++ src/main/java/com/capeelectric/service/impl/AWSEmailService.java
@@ -39,6 +39,9 @@ public class AWSEmailService {
 	@Value("${app.email.multiple.person.third}")
 	private String thirdPersonEmail;
 
+	@Value("${app.email.disable}")
+	private String emailDisable;
+	
 	private static final Logger logger = LoggerFactory.getLogger(AWSEmailService.class);
 
 	@Autowired
@@ -52,60 +55,66 @@ public class AWSEmailService {
 	 */
 	public  void sendEmail(String email, String content) throws MessagingException {
 		
-		logger.debug("Inside AWS Email");
-		final String TO = email; // {YOUR_RECIPIENT_EMAIL_ADDRESS}
+		if(!emailDisable.equalsIgnoreCase("Y")) {
+			logger.debug("Inside AWS Email");
+			final String TO = email; // {YOUR_RECIPIENT_EMAIL_ADDRESS}
 
-		Properties props = new Properties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.host", emailConfig.getSMTP_HOST_NAME());
-		props.put("mail.smtp.auth", "false");
-		props.put("mail.smtp.starttls.enable", "true");
+			Properties props = new Properties();
+			props.put("mail.transport.protocol", "smtp");
+			props.put("mail.smtp.host", emailConfig.getSMTP_HOST_NAME());
+			props.put("mail.smtp.auth", "false");
+			props.put("mail.smtp.starttls.enable", "true");
 
-		Session mailSession = Session.getDefaultInstance(props);
-		mailSession.setDebug(true);
+			Session mailSession = Session.getDefaultInstance(props);
+			mailSession.setDebug(true);
 
-		Transport transport = mailSession.getTransport("smtp");
+			Transport transport = mailSession.getTransport("smtp");
 
-		MimeMessage message = new MimeMessage(mailSession);
+			MimeMessage message = new MimeMessage(mailSession);
 
-		message.setSubject("Welcome to Rush Safety Application");
-		message.setContent(content, "text/plain");
-		message.setSentDate(new Date());
-		message.setFrom(new InternetAddress(FROM));
-		message.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
-		transport.connect(emailConfig.getSMTP_HOST_NAME(), Integer.valueOf(emailConfig.getSMTP_HOST_PORT()), emailConfig.getSMTP_AUTH_USER(), emailConfig.getSMTP_AUTH_PWD());
-		transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
-		transport.close();
+			message.setSubject("Welcome to Rush Safety Application");
+			message.setContent(content, "text/plain");
+			message.setSentDate(new Date());
+			message.setFrom(new InternetAddress(FROM));
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+			transport.connect(emailConfig.getSMTP_HOST_NAME(), Integer.valueOf(emailConfig.getSMTP_HOST_PORT()), emailConfig.getSMTP_AUTH_USER(), emailConfig.getSMTP_AUTH_PWD());
+			transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+			transport.close();
+		}
+		
 
 
 	}
 	
 	public  void sendEmailToAdmin(String content) throws MessagingException {
 
-		logger.debug("Inside AWS Email");
-		final String TO = firstPersonEmail+","+secondPersonEmail+","+thirdPersonEmail; // {YOUR_RECIPIENT_EMAIL_ADDRESS}
+		if(!emailDisable.equalsIgnoreCase("Y")) {
+			logger.debug("Inside AWS Email");
+			final String TO = firstPersonEmail+","+secondPersonEmail+","+thirdPersonEmail; // {YOUR_RECIPIENT_EMAIL_ADDRESS}
 
-		Properties props = new Properties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.host", emailConfig.getSMTP_HOST_NAME());
-		props.put("mail.smtp.auth", "false");
-		props.put("mail.smtp.starttls.enable", "true");
+			Properties props = new Properties();
+			props.put("mail.transport.protocol", "smtp");
+			props.put("mail.smtp.host", emailConfig.getSMTP_HOST_NAME());
+			props.put("mail.smtp.auth", "false");
+			props.put("mail.smtp.starttls.enable", "true");
 
-		Session mailSession = Session.getDefaultInstance(props);
-		mailSession.setDebug(true);
+			Session mailSession = Session.getDefaultInstance(props);
+			mailSession.setDebug(true);
 
-		Transport transport = mailSession.getTransport("smtp");
+			Transport transport = mailSession.getTransport("smtp");
 
-		MimeMessage message = new MimeMessage(mailSession);
+			MimeMessage message = new MimeMessage(mailSession);
 
-		message.setSubject("Approval For Inspector");
-		message.setContent(content, "text/plain");
-		message.setSentDate(new Date());
-		message.setFrom(new InternetAddress(FROM));
-		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(TO) );
-		transport.connect(emailConfig.getSMTP_HOST_NAME(), Integer.valueOf(emailConfig.getSMTP_HOST_PORT()), emailConfig.getSMTP_AUTH_USER(), emailConfig.getSMTP_AUTH_PWD());
-		transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
-		transport.close();
+			message.setSubject("Approval For Inspector");
+			message.setContent(content, "text/plain");
+			message.setSentDate(new Date());
+			message.setFrom(new InternetAddress(FROM));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(TO) );
+			transport.connect(emailConfig.getSMTP_HOST_NAME(), Integer.valueOf(emailConfig.getSMTP_HOST_PORT()), emailConfig.getSMTP_AUTH_USER(), emailConfig.getSMTP_AUTH_PWD());
+			transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+			transport.close();
+		}
+		
 
 
 	}
