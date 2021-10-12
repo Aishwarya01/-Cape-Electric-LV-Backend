b5a13ab966e3d71335a7c094806cf602b9bd9fed
Sivaraju <sivaraju@capeindia.net>
2021-10-12 12:28:28 +0530

    sending pdf in mail

--- src/main/java/com/capeelectric/service/impl/AWSEmailService.java
+++ src/main/java/com/capeelectric/service/impl/AWSEmailService.java
@@ -4,12 +4,20 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -154,5 +162,44 @@ public class AWSEmailService {
 		transport.close();
 
 		}
+	}
+	public void sendEmail(String userName) throws MessagingException {
+		
+		String to = userName;
+		String from = FROM;
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.host", emailConfig.getSMTP_HOST_NAME());
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(emailConfig.getSMTP_AUTH_USER(), emailConfig.getSMTP_AUTH_PWD());
+			}
+		});
+		
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setSubject("Testing Subject");
+			BodyPart messageBodyPart = new MimeBodyPart();
+			messageBodyPart.setText("This is message body");
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(messageBodyPart);
+			messageBodyPart = new MimeBodyPart();
+			String filename = ("Finalreport.pdf");
+
+			DataSource source = new FileDataSource(filename);
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName(filename);
+			multipart.addBodyPart(messageBodyPart);
+			message.setContent(multipart);
+			Transport.send(message);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+			
 	}
 }
