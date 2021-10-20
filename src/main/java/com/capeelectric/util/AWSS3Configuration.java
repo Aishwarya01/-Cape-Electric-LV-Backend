089c168e665ef3b5e995073a9927f407c6efcf99
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-10-20 13:13:00 +0530

    Removed the region in AWS S3 Config

--- src/main/java/com/capeelectric/util/AWSS3Configuration.java
+++ src/main/java/com/capeelectric/util/AWSS3Configuration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -18,9 +19,6 @@ public class AWSS3Configuration {
     @Value("${access.key.secret}")
     private String accessKeySecret;
 
-    @Value("${s3.region.name}")
-    private String s3RegionName;
-
     @Bean
     public AmazonS3 getAmazonS3Client() {
         final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKeyId, accessKeySecret);
@@ -28,7 +26,7 @@ public class AWSS3Configuration {
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-                .withRegion(s3RegionName)
+                .withRegion(Regions.AP_SOUTH_1)
                 .build();
     }
 
