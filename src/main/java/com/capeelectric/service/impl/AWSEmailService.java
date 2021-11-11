7e368316cefcce9cd338c7b8b519767b531298f8
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-11-11 20:35:54 +0530

    Changes related to download the file and attach to PDF

--- src/main/java/com/capeelectric/service/impl/AWSEmailService.java
+++ src/main/java/com/capeelectric/service/impl/AWSEmailService.java
@@ -11,7 +11,6 @@ import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -154,43 +153,48 @@ public class AWSEmailService {
 
 		}
 	}
+	/**
+	 * Download and Send the email to the user
+	 * @param userName
+	 * @throws MessagingException
+	 */
 	public void sendEmailPDF(String userName) throws MessagingException {
 		
-		String to = userName;
-		String from = FROM;
-		Properties props = new Properties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.host", emailConfig.getSMTP_HOST_NAME());
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(emailConfig.getSMTP_AUTH_USER(), emailConfig.getSMTP_AUTH_PWD());
+		if(!emailDisable.equalsIgnoreCase("Y")) {
+			String to = userName;
+			String from = FROM;
+			Properties props = new Properties();
+			props.put("mail.transport.protocol", "smtp");
+			props.put("mail.smtp.host", emailConfig.getSMTP_HOST_NAME());
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			Session mailSession = Session.getDefaultInstance(props);
+
+			try {
+
+				Message message = new MimeMessage(mailSession);
+				Transport transport = mailSession.getTransport("smtp");
+				message.setFrom(new InternetAddress(from));
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+				message.setSubject("Welcome To Rush App Online Services");
+				BodyPart messageBodyPart = new MimeBodyPart();
+				messageBodyPart.setText("Attached is the final report PDF for your kind reference");
+				Multipart multipart = new MimeMultipart();
+				multipart.addBodyPart(messageBodyPart);
+				messageBodyPart = new MimeBodyPart();
+				String filename = ("finalreport.pdf");
+
+				DataSource source = new FileDataSource(filename);
+				messageBodyPart.setDataHandler(new DataHandler(source));
+				messageBodyPart.setFileName(filename);
+				multipart.addBodyPart(messageBodyPart);
+				message.setContent(multipart);
+				transport.connect(emailConfig.getSMTP_HOST_NAME(), Integer.valueOf(Constants.AWS_EMAIL_PORT), emailConfig.getSMTP_AUTH_USER(), emailConfig.getSMTP_AUTH_PWD());
+				transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+				transport.close();
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
 			}
-		});
-		
-		try {
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(from));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-			message.setSubject("Welcome To Rush App Online Services");
-			BodyPart messageBodyPart = new MimeBodyPart();
-			messageBodyPart.setText("Attached is the final report PDF for your kind reference");
-			Multipart multipart = new MimeMultipart();
-			multipart.addBodyPart(messageBodyPart);
-			messageBodyPart = new MimeBodyPart();
-			String filename = ("finalreport.pdf");
-
-			DataSource source = new FileDataSource(filename);
-			messageBodyPart.setDataHandler(new DataHandler(source));
-			messageBodyPart.setFileName(filename);
-			multipart.addBodyPart(messageBodyPart);
-			message.setContent(multipart);
-			Transport.send(message);
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
 		}
-			
 	}
 }
