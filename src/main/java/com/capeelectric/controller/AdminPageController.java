4bf7cc8f7e3dc85e63892e14c6f476706dc97c6a
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2022-02-11 13:15:24 +0530

    Update the reject logic for AdminPageController

--- src/main/java/com/capeelectric/controller/AdminPageController.java
+++ src/main/java/com/capeelectric/controller/AdminPageController.java
@@ -63,7 +63,7 @@ public class AdminPageController {
 							+ "/generateOtp" + ";email=" + register.getUsername());
 		} else {
 			awsEmailService.sendEmail(register.getUsername(),
-					"Your Registration has not Approved, So You Can't Access SOLVE for Safety App");
+					"Your Registration has not Approved since the facility is available only for engineers who completed training with SOLVE");
 		}
 		return new ResponseEntity<String>("Successfully Updated RegisterPermission", HttpStatus.OK);
 	}
