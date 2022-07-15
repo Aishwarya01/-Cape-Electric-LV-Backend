07745933e29d317398c6700b035285f79e61cd8b
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2022-07-15 18:33:21 +0530

    Removed the unused imports

--- src/main/java/com/capeelectric/util/HeaderFooterPageEvent.java
+++ src/main/java/com/capeelectric/util/HeaderFooterPageEvent.java
@@ -1,13 +1,13 @@
 package com.capeelectric.util;
 
 import java.io.IOException;
+
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.ExceptionConverter;
 import com.itextpdf.text.Font;
-import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.BaseFont;
